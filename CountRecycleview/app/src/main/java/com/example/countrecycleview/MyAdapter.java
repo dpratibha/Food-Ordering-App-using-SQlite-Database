5194dde6d6ef6b58707b1c66b587da95cd52f94e
dpratibha <pratibha.j.dhage@gmail.com>
2023-10-26 15:48:18 +0530

    changes

---
+++ CountRecycleview/app/src/main/java/com/example/countrecycleview/MyAdapter.java
@@ -0,0 +1,46 @@
+package com.example.countrecycleview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    Context context;
+List<String> countries;
+
+    public MyAdapter(Context context, List<String> countries) {
+        this.context = context;
+        this.countries = countries;
+    }
+
+
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.list,null);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
